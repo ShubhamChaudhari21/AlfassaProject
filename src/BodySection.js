da5ca4a15d89dc2c2c157368ed6d6fb72f51e8dc
Shubh <shubhamchaudhari21@gmail.com>
2022-01-03 14:30:17 +0530

    Horizontal cards

--- src/BodySection.js
+++ src/BodySection.js
@@ -14,7 +14,7 @@ const responsive = {
   },
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 3,
+    items: 3.5,
   },
   tablet: {
     breakpoint: { max: 1024, min: 700 },
@@ -30,7 +30,8 @@ function BodySection() {
   const [dataStore, setdataStore] = useState([]);
   const [filteredData, setfilteredData] = useState([]);
 
-  useEffect(() => { //Initially load datainto dataStore
+  useEffect(() => {
+    //Initially load datainto dataStore
     axios
       .get("https://www.alfassa.online/feed/")
       .then((result) => convert.xml2js(result.data))
@@ -44,50 +45,98 @@ function BodySection() {
       });
   }, []);
 
-  useEffect(() => { //filter useful data from dataStore
-    let dataItems=[];
-    if(dataStore !== undefined)
-    {
+  useEffect(() => {
+    //filter useful data from dataStore
+    let dataItems = [];
+    if (dataStore !== undefined) {
       dataStore.map((obj) => {
         dataItems.push(
-          (obj.elements).filter( // filter necessary elements from object array
-            (nestedObj)=>{
-              return nestedObj.name==="title" || nestedObj.name==="link" || 
-              nestedObj.name==="pubDate" || nestedObj.name==="content:encoded" || 
-              nestedObj.name==="description"
-            }))
-      })
-      let filterDataArray=[]
-      dataItems.map(element => { // filter and extract specific data
-        let dataObj=
-        {
-            title : (element.filter(el=>{return el.name==="title"}))[0].hasOwnProperty('elements') ? (element.filter(el=>{return el.name==="title"}))[0].elements[0].text : "",
-            link : (element.filter(el=>{return el.name==="link"}))[0].hasOwnProperty('elements') ? (element.filter(el=>{return el.name==="link"}))[0].elements[0].text : "",
-            pubDate : (element.filter(el=>{return el.name==="pubDate"}))[0].hasOwnProperty('elements') ? (element.filter(el=>{return el.name==="pubDate"}))[0].elements[0].text : "",
-            image : (element.filter(el=>{return el.name==="content:encoded"}))[0].hasOwnProperty('elements') ? (element.filter(el=>{return el.name==="content:encoded"}))[0].elements[0].cdata : "",
-            description : (element.filter(el=>{return el.name==="description"}))[0].hasOwnProperty('elements') ? (element.filter(el=>{return el.name==="description"}))[0].elements[0].cdata : ""  
-        }
+          obj.elements.filter(
+            // filter necessary elements from object array
+            (nestedObj) => {
+              return (
+                nestedObj.name === "title" ||
+                nestedObj.name === "link" ||
+                nestedObj.name === "pubDate" ||
+                nestedObj.name === "content:encoded" ||
+                nestedObj.name === "description"
+              );
+            }
+          )
+        );
+      });
+      let filterDataArray = [];
+      dataItems.map((element) => {
+        // filter and extract specific data
+        let dataObj = {
+          title: element
+            .filter((el) => {
+              return el.name === "title";
+            })[0]
+            .hasOwnProperty("elements")
+            ? element.filter((el) => {
+                return el.name === "title";
+              })[0].elements[0].text
+            : "",
+          link: element
+            .filter((el) => {
+              return el.name === "link";
+            })[0]
+            .hasOwnProperty("elements")
+            ? element.filter((el) => {
+                return el.name === "link";
+              })[0].elements[0].text
+            : "",
+          pubDate: element
+            .filter((el) => {
+              return el.name === "pubDate";
+            })[0]
+            .hasOwnProperty("elements")
+            ? element.filter((el) => {
+                return el.name === "pubDate";
+              })[0].elements[0].text
+            : "",
+          image: element
+            .filter((el) => {
+              return el.name === "content:encoded";
+            })[0]
+            .hasOwnProperty("elements")
+            ? element.filter((el) => {
+                return el.name === "content:encoded";
+              })[0].elements[0].cdata
+            : "",
+          description: element
+            .filter((el) => {
+              return el.name === "description";
+            })[0]
+            .hasOwnProperty("elements")
+            ? element.filter((el) => {
+                return el.name === "description";
+              })[0].elements[0].cdata
+            : "",
+        };
         filterDataArray.push(dataObj);
       });
 
       //Clean description in object
-      filterDataArray.map(item=>{
-       item["description"]=(item.description).replace(/<[^>]*>?/gm, '');
-      })
-       
+      filterDataArray.map((item) => {
+        item["description"] = item.description.replace(/<[^>]*>?/gm, "");
+      });
+
       //extract image link
-      filterDataArray.map(item=>{
-        var slicedString=(item.image).slice((item.image).indexOf('src'))
+      filterDataArray.map((item) => {
+        var slicedString = item.image.slice(item.image.indexOf("src"));
         item.image = slicedString.substring(
-        slicedString.indexOf('src') + 5, 
-        slicedString.indexOf('class') -1 );
-      })
+          slicedString.indexOf("src") + 5,
+          slicedString.indexOf("class") - 1
+        );
+      });
 
       // console.log(filterDataArray)
-      setfilteredData(filterDataArray)
+      setfilteredData(filterDataArray);
     }
   }, [dataStore]);
-  
+
   return (
     <div className="BodySection">
       <div className="title"> Ultimi Articoli Alfassa Online</div>
@@ -102,18 +151,17 @@ function BodySection() {
       </div>
       <div>
         <Carousel responsive={responsive}>
-        {
-            filteredData!==undefined ? filteredData.map((item)=>{ 
-              return (
-              <CardSection
-                image={item.image}
-                title={item.title}
-                content={item.description}
-              />)           
-            })
-            :
-            ""
-        }
+          {filteredData !== undefined
+            ? filteredData.map((item) => {
+                return (
+                  <CardSection
+                    image={item.image}
+                    title={item.title}
+                    content={item.description}
+                  />
+                );
+              })
+            : ""}
         </Carousel>
       </div>
     </div>
--- src/CardSection.js
+++ src/CardSection.js
@@ -5,24 +5,13 @@ function CardSection(props) {
   return (
     <div className="CardSection d-flex justify-content-center">
       <div className="card">
-        {(props.image).length !== 0 ?<img className="cardImage" src={props.image}/> : <img style={{"height":"50px"}}/>}
-        <div className="cardBrand d-flex">
-          <img
-            src="https://www.alfassa.online/wp-content/uploads/2021/10/cropped-Rosa-ALFASSA-scaled-1-32x32.jpg"
-            alt=""
-            class="brandImage"
-          />
-          <div className="brandTitle ">
-            <p className="m-0 title">Alfassa</p>
-            <p className="m-0 meta d-flex align-items-center">
-              <div>@Alfassa</div>
-              <div style={{ fontWeight: "900" }}>&bull;</div>
-              <div>4 days ago</div>
-            </p>
+        <div className="cardTitle">{props.title}</div>
+        {props.image.length !== 0 && props.content.length !== 0 ? (
+          <div className="d-flex cardContentBody">
+            <img className="cardImage" src={props.image} />
+            <div className="cardContent text-justify">{props.content}</div>
           </div>
-        </div>
-        <div className="cardTitle text-center">{props.title}</div>
-        <div className="cardContent text-center">{props.content}</div>
+        ) : null}
       </div>
     </div>
   );
