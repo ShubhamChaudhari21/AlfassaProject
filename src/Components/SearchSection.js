6e9117e624e1d3fd961802ba92d3884a568ff183
Shubh <shubhamchaudhari21@gmail.com>
2022-01-16 15:55:21 +0530

    icon tray linked

--- src/Components/SearchSection.js
+++ src/Components/SearchSection.js
@@ -14,38 +14,54 @@ function SearchSection(props) {
         </p>
         <SearchBar />
         <div className="iconTray d-flex col justify-content-center align-items-center flex-wrap">
-          <div className="d-flex flex-column align-items-center">
-            <img
-              className="alfaSocialIcon"
-              alt="alfaSocialIcon"
-              src="https://www.enersat.it/wp-content/uploads/2022/01/Alfa-Social-1.png"
-            />
-            <p>AlfaSocial</p>
-          </div>
-          <div className="d-flex flex-column align-items-center">
-            <img
-              className="alfaTubeIcon"
-              alt="alfaTubeIcon"
-              src="https://www.enersat.it/wp-content/uploads/2022/01/Alfa-Tube.png"
-            ></img>
-            <p>AlfaTube</p>
-          </div>
-          <div className="d-flex flex-column align-items-center">
-            <img
-              className="newsIcon"
-              alt="newsIcon"
-              src="https://www.enersat.it/wp-content/uploads/2022/01/Alfa-Media.png"
-            ></img>
-            <p>News</p>
-          </div>
-          <div className="d-flex flex-column align-items-center">
-            <img
-              className="alfassaIcon"
-              alt="alfassaIcon"
-              src="https://www.enersat.it/wp-content/uploads/2022/01/ALFASSA.png"
-            ></img>
-            <p>Alfassa</p>
-          </div>
+          <a
+            href="https://www.alfassasocial.it/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="d-flex flex-column align-items-center">
+              <img
+                className="alfaSocialIcon"
+                alt="alfaSocialIcon"
+                src="https://www.enersat.it/wp-content/uploads/2022/01/Alfa-Social-1.png"
+              />
+              <p>AlfaSocial</p>
+            </div>
+          </a>
+          <a
+            href="https://www.alfassatube.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="d-flex flex-column align-items-center">
+              <img
+                className="alfaTubeIcon"
+                alt="alfaTubeIcon"
+                src="https://www.enersat.it/wp-content/uploads/2022/01/Alfa-Tube.png"
+              ></img>
+              <p>AlfaTube</p>
+            </div>
+          </a>
+          <a href="https://www.alfassa.online" target="_blank" rel="noreferrer">
+            <div className="d-flex flex-column align-items-center">
+              <img
+                className="newsIcon"
+                alt="newsIcon"
+                src="https://www.enersat.it/wp-content/uploads/2022/01/Alfa-Media.png"
+              ></img>
+              <p>News</p>
+            </div>
+          </a>
+          <a href="https://www.alfassa.net/" target="_blank" rel="noreferrer">
+            <div className="d-flex flex-column align-items-center">
+              <img
+                className="alfassaIcon"
+                alt="alfassaIcon"
+                src="https://www.enersat.it/wp-content/uploads/2022/01/ALFASSA.png"
+              ></img>
+              <p>Alfassa</p>
+            </div>
+          </a>
         </div>
 
         <p className="footer mb-0 text-center">
